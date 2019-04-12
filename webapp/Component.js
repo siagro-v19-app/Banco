6819f3bd0d6517446adc15a726787b86690d3b1d
matusca96 <gomesloud@gmail.com>
2019-04-12 10:26:49 -0300

    Cadastro de Banco já atualizado.

---
+++ webapp/Component.js
@@ -0,0 +1,42 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"br/com/idxtecBanco/model/models",
+	"br/com/idxtecBanco/services/ErrorHandler"
+], function(UIComponent, Device, models, ErrorHandler) {
+	"use strict";
+
+	return UIComponent.extend("br.com.idxtecBanco.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function() {
+			
+			this._oErrorHandler = new ErrorHandler(this);
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+			this.setModel(models.createViewModel(), "view");
+		},
+		
+		destroy: function(){
+			this._oErrorHandler.destroy();
+			
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+		
+		getContentDensityClass: function(){
+			if(!this._sContentDensityClass){
+				if(!sap.ui.Device.support.touch){
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+	});
+});
---
+++ webapp/controller/Banco.controller.js
@@ -0,0 +1,223 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"sap/ui/model/json/JSONModel",
+	"br/com/idxtecBanco/services/Session",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, MessageBox, JSONModel, Session, Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("br.com.idxtecBanco.controller.Banco", {
+		onInit: function(){
+			var oJSONModel = new JSONModel();
+			
+			this._operacao = null;
+			this._sPath = null;
+
+			this.getOwnerComponent().setModel(oJSONModel, "model");
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+			
+
+			this.getModel().attachMetadataLoaded(function(){
+				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+				var oView = this.getView();
+				var oTable = oView.byId("tableBanco");
+				var oColumn = oView.byId("columnNome");
+				
+				oTable.sort(oColumn);
+				oView.byId("tableBanco").getBinding("rows").filter(oFilter, "Application");
+			});
+		},
+		
+		filtraNome: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oFilter2 = new Filter("Nome", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter1,
+				oFilter2
+			];
+
+			this.getView().byId("tableBanco").getBinding("rows").filter(aFilters, "Application");
+		},
+
+		onRefresh: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			oModel.refresh(true);
+			this.getView().byId("tableBanco").clearSelection();
+		},
+		
+		onIncluir: function(){
+			var oDialog = this._criarDialog();
+			var oTable = this.byId("tableBanco");
+			var oJSONModel = this.getOwnerComponent().getModel("model");
+			var oViewModel = this.getModel("view");
+			
+			oViewModel.setData({
+				titulo: "Inserir Banco",
+				codigoEdit: true,
+				msgSalvar: "Banco inserido com sucesso!"
+			});
+			
+			this._operacao = "incluir";
+			
+			var oNovoBanco = {
+				"Codigo": "",
+				"Nome": "",
+				"Empresa" : Session.get("EMPRESA_ID"),
+				"Usuario": Session.get("USUARIO_ID"),
+				"EmpresaDetails": { __metadata: { uri: "/Empresas(" + Session.get("EMPRESA_ID") + ")"}},
+				"UsuarioDetails": { __metadata: { uri: "/Usuarios(" + Session.get("USUARIO_ID") + ")"}}
+			};
+			
+			oJSONModel.setData(oNovoBanco);
+			
+			oTable.clearSelection();
+			oDialog.open();
+		},
+		
+		onEditar: function(){
+			var oDialog = this._criarDialog();
+			var oTable = this.byId("tableBanco");
+			var nIndex = oTable.getSelectedIndex();
+			var oModel = this.getOwnerComponent().getModel();
+			var oJSONModel = this.getOwnerComponent().getModel("model");
+			var oViewModel = this.getModel("view");
+			
+			oViewModel.setData({
+				titulo: "Editar Banco",
+				codigoEdit: false,
+				msgSalvar: "Banco alterado com sucesso!"
+			});
+			
+			this._operacao = "editar";
+			
+			if(nIndex === -1){
+				MessageBox.warning("Selecione um banco da tabela!");
+				return;
+			}
+			
+			var oContext = oTable.getContextByIndex(nIndex);
+			this._sPath = oContext.sPath;
+			
+			oModel.read(oContext.sPath, {
+				success: function(oData){
+					oJSONModel.setData(oData);
+				}
+			});
+			
+			oTable.clearSelection();
+			oDialog.open();
+		},
+		
+		onRemover: function(){
+			var that = this;
+			var oTable = this.byId("tableBanco");
+			var nIndex = oTable.getSelectedIndex();
+			
+			if(nIndex === -1){
+				MessageBox.warning("Selecione um banco na tabela!");
+				return;
+			}
+			
+			MessageBox.confirm("Deseja remover esse banco?", {
+				onClose: function(sResposta){
+					if(sResposta === "OK"){
+						that._remover(oTable, nIndex);
+						MessageBox.success("Banco removido com sucesso!");
+					}
+				} 
+			});
+		},
+		
+		onSaveDialog: function(){
+			if (this._checarCampos(this.getView())) {
+				MessageBox.warning("Preencha todos os campos obrigatórios!");
+				return;
+			}
+			if(this._operacao === "incluir"){
+				this._createBanco();
+				this.getView().byId("BancoDialog").close();
+			} else if(this._operacao === "editar"){
+				this._updateBanco();
+				this.getView().byId("BancoDialog").close();
+			} 
+		},
+		
+		onCloseDialog: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			
+			if(oModel.hasPendingChanges()){
+				oModel.resetChanges();
+			}
+			
+			this.byId("BancoDialog").close();
+		},
+		
+		_remover: function(oTable, nIndex){
+			var oModel = this.getOwnerComponent().getModel();
+			var oContext = oTable.getContextByIndex(nIndex);
+			
+			oModel.remove(oContext.sPath, {
+				success: function(){
+					oModel.refresh(true);
+					oTable.clearSelection();
+				}
+			});
+		},
+		
+		_getDados: function(){
+			var oJSONModel = this.getOwnerComponent().getModel("model");
+			var oDados = oJSONModel.getData();
+			return oDados;
+		},
+		
+		_createBanco: function(){
+			var oModel = this.getOwnerComponent().getModel();
+	
+			oModel.create("/Bancos", this._getDados(), {
+				success: function() {
+					MessageBox.success("Banco inserido com sucesso!");
+					oModel.refresh(true);
+				}
+			});
+		},
+		
+		_updateBanco: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			
+			oModel.update(this._sPath, this._getDados(), {
+				success: function(){
+					MessageBox.success("Banco alterado com sucesso!");
+					oModel.refresh(true);
+				}
+			});
+		},
+		
+		_criarDialog: function(){
+			var oView = this.getView();
+			var oDialog = this.byId("BancoDialog");
+			
+			if(!oDialog){
+				oDialog = sap.ui.xmlfragment(oView.getId(), "br.com.idxtecBanco.view.BancoDialog", this);
+				oView.addDependent(oDialog); 
+			}
+			return oDialog;
+		},
+		
+		_checarCampos: function(oView){
+			if(oView.byId("codigo").getValue() === "" || oView.byId("nome").getValue() === ""){
+				return true;
+			} else{
+				return false; 
+			}
+		},
+		
+		getModel: function(sModel) {
+			return this.getOwnerComponent().getModel(sModel);
+		}
+	});
+});
